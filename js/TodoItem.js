e6fbf7f0999dfe91d8c42d43be1b5379b4814271
gdejules <gd.juliarthaa@gmail.com>
2026-04-15 23:28:39 +0800

    Add toggleCompleted method

---
+++ js/TodoItem.js
@@ -0,0 +1,10 @@
+export default class TodoItem {
+    constructor() {
+        this.text = this.text;
+        this.completed = false;
+    }
+    // TODO: Implement toggleCompleted method
+    toggleCompleted() {
+        return this.completed = true;
+    }
+}
