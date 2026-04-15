c5026cfebea300c155810330fae4453b4ca95d43
gdejules <gd.juliarthaa@gmail.com>
2026-04-15 23:23:08 +0800

    Add addTodo, removeTodo, toggleTodo method

--- js/TodoList.js
+++ js/TodoList.js
@@ -3,8 +3,15 @@ export default class TodoList {
         this.todos = [];
     }
     // TODO: Implement addTodo method
-
+    addTodo() {
+        return this.todos.values;
+    }
     // TODO: Implement removeTodo method
-
+    removeTodo() {
+        this.todos.remove();
+    }
     // TODO: Implement toggleTodo method
+    toggleTodo() {
+        this.style.textDecoration = "line-through";
+    }
 }
